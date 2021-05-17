d95bc9a9d7ca73884ef409032eba2987d3ad3eb0
Guilherme Henrique <guilherme_hrq99@outlook.com>
2021-05-17 20:31:44 -0300

    Cliente, TipoCliente, Telefones e Enderecos

--- src/main/java/com/jamal/cursomc/CursomcApplication.java
+++ src/main/java/com/jamal/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jamal.cursomc.domain.Categoria;
 import com.jamal.cursomc.domain.Cidade;
+import com.jamal.cursomc.domain.Cliente;
+import com.jamal.cursomc.domain.Endereco;
 import com.jamal.cursomc.domain.Estado;
 import com.jamal.cursomc.domain.Produto;
+import com.jamal.cursomc.domain.enums.TipoCliente;
 import com.jamal.cursomc.repositories.CategoriaRepository;
 import com.jamal.cursomc.repositories.CidadeRepository;
+import com.jamal.cursomc.repositories.ClienteRepository;
+import com.jamal.cursomc.repositories.EnderecoRepository;
 import com.jamal.cursomc.repositories.EstadoRepository;
 import com.jamal.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -66,5 +77,16 @@ public class CursomcApplication implements CommandLineRunner {
 	    
 	    estadoRepository.saveAll(Arrays.asList(est1, est2));
 	    cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+	    
+	    Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+	    cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+	    
+	    Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+	    Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777015", cli1, c2);
+	    
+	    cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+	    
+	    clienteRepository.saveAll(Arrays.asList(cli1));
+	    enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
--- src/main/java/com/jamal/cursomc/domain/Estado.java
+++ src/main/java/com/jamal/cursomc/domain/Estado.java
@@ -20,7 +20,7 @@ public class Estado implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@OneToMany(mappedBy="estado")
+	@OneToMany(mappedBy = "estado")
 	private List<Cidade> cidades = new ArrayList<>();
 	
 	public Estado() {}
