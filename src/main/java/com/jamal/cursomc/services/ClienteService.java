8e278733e9717e80420648f8f6ddb2032b463b60
Guilherme Henrique <guilherme_hrq99@outlook.com>
2021-05-17 20:46:30 -0300

    Enpoint /clientes/{id}

---
+++ src/main/java/com/jamal/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.jamal.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jamal.cursomc.domain.Cliente;
+import com.jamal.cursomc.repositories.ClienteRepository;
+import com.jamal.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
