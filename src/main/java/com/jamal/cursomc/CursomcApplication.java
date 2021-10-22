0ce77579346111ec3c0fe27a1fe51eb6f57410cc
Guilherme Henrique <guilherme_hrq99@outlook.com>
2021-10-21 22:01:09 -0300

    Pedido, Pagamento, EstadoPagmento

--- src/main/java/com/jamal/cursomc/CursomcApplication.java
+++ src/main/java/com/jamal/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.jamal.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.jamal.cursomc.domain.Cidade;
 import com.jamal.cursomc.domain.Cliente;
 import com.jamal.cursomc.domain.Endereco;
 import com.jamal.cursomc.domain.Estado;
+import com.jamal.cursomc.domain.Pagamento;
+import com.jamal.cursomc.domain.PagamentoComBoleto;
+import com.jamal.cursomc.domain.PagamentoComCartao;
+import com.jamal.cursomc.domain.Pedido;
 import com.jamal.cursomc.domain.Produto;
+import com.jamal.cursomc.domain.enums.EstadoPagamento;
 import com.jamal.cursomc.domain.enums.TipoCliente;
 import com.jamal.cursomc.repositories.CategoriaRepository;
 import com.jamal.cursomc.repositories.CidadeRepository;
 import com.jamal.cursomc.repositories.ClienteRepository;
 import com.jamal.cursomc.repositories.EnderecoRepository;
 import com.jamal.cursomc.repositories.EstadoRepository;
+import com.jamal.cursomc.repositories.PagamentoRepository;
+import com.jamal.cursomc.repositories.PedidoRepository;
 import com.jamal.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -88,5 +102,22 @@ public class CursomcApplication implements CommandLineRunner {
 	    
 	    clienteRepository.saveAll(Arrays.asList(cli1));
 	    enderecoRepository.saveAll(Arrays.asList(e1, e2));
+	    
+	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+	    
+	    Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+	    Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+	    
+	    Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+	    ped1.setPagamento(pagto1);
+	    
+	    Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+	    ped2.setPagamento(pagto2);
+	    
+	    cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+	    
+	    pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+	    pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+	    
 	}
 }
--- src/main/java/com/jamal/cursomc/domain/Cliente.java
+++ src/main/java/com/jamal/cursomc/domain/Cliente.java
@@ -38,6 +38,11 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	// Lista de pedidos do cliente (1:N)
+	// Coleções não entram no construtor
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -104,6 +109,14 @@ public class Cliente implements Serializable {
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -129,6 +142,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
 	
 }
