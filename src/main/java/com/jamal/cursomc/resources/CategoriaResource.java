3dd4b75bd8a694f5e508bfb5a514ff22fdd772b9
Guilherme Assis <guilherme_hrq99@outlook.com>
2021-10-30 14:42:30 -0300

    Inserindo nova Categoria com POST

--- src/main/java/com/jamal/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jamal/cursomc/resources/CategoriaResource.java
@@ -2,13 +2,13 @@ package com.jamal.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jamal.cursomc.domain.Categoria;
 import com.jamal.cursomc.services.CategoriaService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -22,5 +22,17 @@ public class CategoriaResource {
 		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		// A operação "save" retorna o objeto salvo, por isso o "obj = ..."
+		obj = service.insert(obj);
+
+		// Criando URI para ser retornado junto ao HTTP code ("/categorias/{id}")
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		// Retorna o status code 201 + URI gerado
+		return ResponseEntity.created(uri).build();
 	}
 }
