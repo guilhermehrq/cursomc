8597c257e60ba3946ba057a09e4f61090b60c1a1
Guilherme Assis <guilherme_hrq99@outlook.com>
2021-11-25 23:32:33 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/jamal/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jamal/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.jamal.cursomc.resources;
 
 import com.jamal.cursomc.domain.Categoria;
+import com.jamal.cursomc.dto.CategoriaDTO;
 import com.jamal.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -40,7 +43,7 @@ public class CategoriaResource {
 		// Seta o ID do parametro, apenas para garantir que está sendo atualizado o mesmo objeto
 		obj.setId(id);
 
-		obj = service.update(obj);
+		service.update(obj);
 
 		// Retorna um response vazio
 		return ResponseEntity.noContent().build();
@@ -52,5 +55,14 @@ public class CategoriaResource {
 
 		// Retorna um response vazio
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDto);
 	}
 }
