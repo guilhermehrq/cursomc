890aee6824ac0a30f60ca3286aad8ea7bc9fa89b
Guilherme Assis <guilherme_hrq99@outlook.com>
2021-12-14 21:03:16 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/jamal/cursomc/services/ClienteService.java
+++ src/main/java/com/jamal/cursomc/services/ClienteService.java
@@ -1,8 +1,17 @@
 package com.jamal.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
+import ch.qos.logback.core.net.server.Client;
+import com.jamal.cursomc.domain.Cliente;
+import com.jamal.cursomc.dto.ClienteDTO;
+import com.jamal.cursomc.services.exceptions.DataIntegrityException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.jamal.cursomc.domain.Cliente;
@@ -20,4 +29,50 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		// Valida se o ID da alteração de fato existe no banco
+		Cliente newObj = find(obj.getId());
+
+		updateData(newObj, obj);
+
+		// Mesma função do "insert", mas agora com um ID válido
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		// Validando se o objeto a ser deletado realmente existe
+		find(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente porque há entidades relacionadas!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 }
